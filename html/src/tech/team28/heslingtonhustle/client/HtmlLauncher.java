f0dbfc4c5ed91900560137f14c9622ab587f5f6e
jmcb <joelsgp@protonmail.com>
2024-02-23 15:10:23 +0000

    More substantial project setup
    Give project a proper name, remove android and ios for now

--- html/src/tech/team28/heslingtonhustle/client/HtmlLauncher.java
+++ html/src/tech/team28/heslingtonhustle/client/HtmlLauncher.java
@@ -1,9 +1,9 @@
-package com.mygdx.game.client;
+package tech.team28.heslingtonhustle.client;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import tech.team28.heslingtonhustle.HeslingtonHustle;
 
 public class HtmlLauncher extends GwtApplication {
 
@@ -17,6 +17,6 @@ public class HtmlLauncher extends GwtApplication {
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new MyGdxGame();
+                return new HeslingtonHustle();
         }
 }
