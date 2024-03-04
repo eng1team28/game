ef7a727bf377eef2cb7e60eb04ea51c9f8a220cc
jmcb <joelsgp@protonmail.com>
2024-03-04 14:54:30 +0000

    Spaces for indentation

--- core/src/tech/team28/heslingtonhustle/GameScreen.java
+++ core/src/tech/team28/heslingtonhustle/GameScreen.java
@@ -6,96 +6,91 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
 
 public class GameScreen implements Screen {
-	final HeslingtonHustle game;
+    final HeslingtonHustle game;
 
-	private OrthographicCamera camera;
+    private OrthographicCamera camera;
 
-	Texture playerImage;
-	private Rectangle player;
+    Texture playerImage;
+    private Rectangle player;
 
-	private static final int SCREEN_WIDTH = 1920;
-	private static final int SCREEN_HEIGHT = 1080;
+    private static final int SCREEN_WIDTH = 1920;
+    private static final int SCREEN_HEIGHT = 1080;
 
-	// TODO - Move all player logic into a Player class, create and assign movementComponent (Composition)
-	private static final int PLAYER_WIDTH = 32;
-	private static final int PLAYER_HEIGHT = 64;
-	private static final float PLAYER_SPEED = 1000;
-	private static final String PLAYER_TEXTURE = "player.png";
+    // TODO - Move all player logic into a Player class, create and assign movementComponent
+    // (Composition)
+    private static final int PLAYER_WIDTH = 32;
+    private static final int PLAYER_HEIGHT = 64;
+    private static final float PLAYER_SPEED = 1000;
+    private static final String PLAYER_TEXTURE = "player.png";
 
-	public GameScreen (final HeslingtonHustle game) {
-		this.game = game;
+    public GameScreen(final HeslingtonHustle game) {
+        this.game = game;
 
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-		playerImage = new Texture(PLAYER_TEXTURE);
-		player = new Rectangle();
-		player.x = (float) SCREEN_WIDTH / 2 - (float) PLAYER_WIDTH / 2;
-		player.y = (float) SCREEN_HEIGHT / 2 - (float) PLAYER_HEIGHT /2;
-		player.width = PLAYER_WIDTH;
-		player.height = PLAYER_HEIGHT;
-	}
+        playerImage = new Texture(PLAYER_TEXTURE);
+        player = new Rectangle();
+        player.x = (float) SCREEN_WIDTH / 2 - (float) PLAYER_WIDTH / 2;
+        player.y = (float) SCREEN_HEIGHT / 2 - (float) PLAYER_HEIGHT / 2;
+        player.width = PLAYER_WIDTH;
+        player.height = PLAYER_HEIGHT;
+    }
 
-	@Override
-	public void render (float delta) {
-		ScreenUtils.clear(0, 0, 0, 1);
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
 
-		camera.update();
-		game.batch.setProjectionMatrix(camera.combined);
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
 
-		game.batch.begin();
-		game.batch.draw(playerImage, player.x, player.y);
-		game.batch.end();
+        game.batch.begin();
+        game.batch.draw(playerImage, player.x, player.y);
+        game.batch.end();
 
-		Vector2 inputVector = new Vector2();
-		inputVector.setZero();
-		inputVector.add(Gdx.input.isKeyPressed(Input.Keys.LEFT) ? -1 : 0, Gdx.input.isKeyPressed(Input.Keys.UP) ? 1 : 0);
-		inputVector.add(Gdx.input.isKeyPressed(Input.Keys.RIGHT) ? 1 : 0, Gdx.input.isKeyPressed(Input.Keys.DOWN) ? -1 : 0);
-		// Normalize vector so that diagonal movement has the same magnitude
-		inputVector.nor();
+        Vector2 inputVector = new Vector2();
+        inputVector.setZero();
+        inputVector.add(
+                Gdx.input.isKeyPressed(Input.Keys.LEFT) ? -1 : 0,
+                Gdx.input.isKeyPressed(Input.Keys.UP) ? 1 : 0);
+        inputVector.add(
+                Gdx.input.isKeyPressed(Input.Keys.RIGHT) ? 1 : 0,
+                Gdx.input.isKeyPressed(Input.Keys.DOWN) ? -1 : 0);
+        // Normalize vector so that diagonal movement has the same magnitude
+        inputVector.nor();
 
-		// Move the player based on input and PLAYER_SPEED
-		//  * deltaTime -> Frame-rate independent
-		Vector2 displacementVector = inputVector.scl(PLAYER_SPEED * Gdx.graphics.getDeltaTime());
-		player.setPosition(player.getPosition(new Vector2()).add(displacementVector));
+        // Move the player based on input and PLAYER_SPEED
+        //  * deltaTime -> Frame-rate independent
+        Vector2 displacementVector = inputVector.scl(PLAYER_SPEED * Gdx.graphics.getDeltaTime());
+        player.setPosition(player.getPosition(new Vector2()).add(displacementVector));
 
-		// Clamp player to screen
-		player.x = MathUtils.clamp(player.x, 0, SCREEN_WIDTH - PLAYER_WIDTH);
-		player.y = MathUtils.clamp(player.y, 0, SCREEN_HEIGHT - PLAYER_HEIGHT);
-	}
+        // Clamp player to screen
+        player.x = MathUtils.clamp(player.x, 0, SCREEN_WIDTH - PLAYER_WIDTH);
+        player.y = MathUtils.clamp(player.y, 0, SCREEN_HEIGHT - PLAYER_HEIGHT);
+    }
 
-	@Override
-	public void resize(int width, int height) {
+    @Override
+    public void resize(int width, int height) {}
 
-	}
+    @Override
+    public void show() {}
 
-	@Override
-	public void show () {
+    @Override
+    public void hide() {}
 
-	}
+    @Override
+    public void pause() {}
 
-	@Override
-	public void hide() {
+    @Override
+    public void resume() {}
 
-	}
-
-	@Override
-	public void pause() {
-
-	}
-
-	@Override
-	public void resume() {
-
-	}
-
-	@Override
-	public void dispose () {
-		playerImage.dispose();
-	}
+    @Override
+    public void dispose() {
+        playerImage.dispose();
+    }
 }
--- desktop/src/tech/team28/heslingtonhustle/DesktopLauncher.java
+++ desktop/src/tech/team28/heslingtonhustle/DesktopLauncher.java
@@ -3,13 +3,14 @@ package tech.team28.heslingtonhustle;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM
+// argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(0);
-		config.setWindowedMode(1920, 1080);
-		config.setTitle("Heslington Hustle");
-		new Lwjgl3Application(new HeslingtonHustle(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(0);
+        config.setWindowedMode(1920, 1080);
+        config.setTitle("Heslington Hustle");
+        new Lwjgl3Application(new HeslingtonHustle(), config);
+    }
 }
