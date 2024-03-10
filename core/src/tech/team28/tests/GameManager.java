9fb5667f57ae50e5debdd2af88e7e29cb62140f4
ASp2004 <alexspencer2004@icloud.com>
2024-03-10 20:26:36 +0000

    Basis of unit tests

---
+++ core/src/tech/team28/tests/GameManager.java
@@ -0,0 +1,10 @@
+package tech.team28.tests;
+
+import tech.team28.heslingtonhustle.Player;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameManager {
+    
+}
---
+++ core/src/tech/team28/tests/InteractableTest.java
@@ -0,0 +1,12 @@
+package tech.team28.tests;
+
+import tech.team28.heslingtonhustle.Player;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//Every interactable subclass will be tested here unless we really want specificity in our tests
+
+public class InteractableTest {
+    
+}
