8dc809abc866a7979b57f281a87eba7b3da8b576
jmcb <joelsgp@protonmail.com>
2024-03-19 12:18:04 +0000

    Fix the font
    Why did you do that

--- core/src/tech/team28/heslingtonhustle/GameScreen.java
+++ core/src/tech/team28/heslingtonhustle/GameScreen.java
@@ -126,12 +126,8 @@ public class GameScreen implements Screen {
         table.top().left();
         table.pad(10f);
 
-        BitmapFont currentFont = new BitmapFont();
-
-        currentFont.getData().setScale(3f, 3f);
-
+        BitmapFont currentFont = game.font;
         Label.LabelStyle sillyStyle = new Label.LabelStyle(currentFont, Color.WHITE);
-        sillyStyle.font.getData().setScale(0.4f);
         // The next couple of lines initialises labels for displaying game information
         dayLabel = new Label("", sillyStyle);
         timeLabel = new Label("", sillyStyle);
