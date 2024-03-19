1475366c4107eec2f61d125ffd3d6a626c274d58
ASp2004 <alexspencer2004@icloud.com>
2024-03-19 16:07:39 +0000

    Fixed end of day cutscene

--- core/src/tech/team28/heslingtonhustle/GameManager.java
+++ core/src/tech/team28/heslingtonhustle/GameManager.java
@@ -157,7 +157,7 @@ public class GameManager {
 
         boolean examWin;
         examWin = player.getIntelligence() >= 60;
-        player.setPosition(150, 0); // Move player into a position, so they can see the result
+        player.setPosition(950, 500); // Move player into a position, so they can see the result
         this.game.examCutscene(examWin);
     }
 }
